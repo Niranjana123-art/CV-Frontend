935b187afb84be59d2de267461d0c28775fde1a4
Anuj Kumar Sharma <niranjanamor@gmail.com>
2023-04-23 01:47:21 +0530

    landing page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
-
+// import Navbar from '../../components/Navbar/Navbar'
+import './Landing.css'
 const Landing = () => {
   return (
-    <div>
-        <Navbar/>
-        Nandini
+    <div className='home_container'>
+      <div className='home_title'>
+        
+        Let <span className='bold_span'>Recruiters Find</span> You!
+      </div>
     </div>
   )
 }
