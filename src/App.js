0ff02e783e46c8b141b96e07ec7f33e494984217
SnehaDevan <sdkvekm2@gmail.com>
2023-04-23 10:30:59 +0530

    Comleted SignUp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Landing from './pages/Landing/Landing';
+
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+      
     </div>
   );
 }
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+//import Navbar from '../../components/Navbar/Navbar'
 
 const Landing = () => {
   return (
     <div>
-        <Navbar/>
-        Nandini
-    </div>
+        {/*<Navbar/>
+        Nandini*/}
+  </div>
   )
 }
 
