2fd6385c3b386a500ca90325ca069cff24a23ef1
Anuj Kumar Sharma <niranjanamor@gmail.com>
2023-04-24 13:05:15 +0530

    navbar completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+
 import Landing from "./pages/Landing/Landing";
 import Login from "./pages/Login/Login";
 import PersonalitySuccess from "./pages/PersonalitySuccess/PersonalitySuccess";
-
+import Navbar from "./components/Navbar/Navbar";
 function App() {
   return (
-    <BrowserRouter>
-       <Routes>    
-            <Route path="/" element={<Landing />} />
-            <Route path="/Login" element={<Login />} />
-            <Route path="/PersonlitySucess" element={<Login />} />
-            <Route path="/PersonalitySuccess" element={<PersonalitySuccess/>} />
-      </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <Navbar/>    
+        <BrowserRouter>
+        <Routes>    
+              <Route path="/" element={<Landing />} />
+              <Route path="/Login" element={<Login />} />
+              <Route path="/PersonlitySucess" element={<Login />} />
+              <Route path="/PersonalitySuccess" element={<PersonalitySuccess/>} />
+              <Route path="/Navbar" element={<Navbar/>} />
+           
+        </Routes>
+        </BrowserRouter>
+    </div>
   );
 }
 
