f8775e3bafa05eb8735f7a0b63ea24cddafaf24f
SnehaDevan <sdkvekm2@gmail.com>
2023-05-07 16:48:23 +0530

    Integration Of DetailAddTwo Done

--- src/pages/DetailAddTwo/DetailAddTwo.js
+++ src/pages/DetailAddTwo/DetailAddTwo.js
@@ -9,68 +9,12 @@ import {FaPhoneSquareAlt} from "react-icons/fa"
 import {FaEnvelope} from "react-icons/fa"
 import {FaLinkedin} from "react-icons/fa"
 
-
-// const DetailAddTwo = () => {
-//   return (
-    // <div className='DetailAddTwo_Container'>
-         
-    //      <div className='DetailAddTwo_Box'>
-           
-    //        <div className='DetailAddTwo_Content'>
-             
-    //           <div className='Additional_info'>
-              
-    //           <div className='Additional_info_heading'>
-    //           <h3>Additional Information</h3></div>
-
-    //           <div className='Additonal_info_content'>
-    //           <form>
-    //              <label><FaPhoneSquareAlt size={30} color='black'/>Phone No:<input type='PhoneNo'/></label>
-    //              <label><FaEnvelope size={30} color='black'/>E-mail Id: <input type='EmailId'/></label>
-    //              <label><FaLinkedin size={30}  color='black'/>LinkedIn Profile:<input type='LinkedIn'/></label>
-    //           </form>
-    //           </div>
-
-    //           </div>
-
-    //           <div className='UploadResume'>
-                
-    //             <div className='UploadResume_heading'>
-    //             <h3>Upload Resume</h3></div>
-
-    //             <div className='UploadResume_content'>
-    //             <input type="file" id="files" class="hidden"/>
-
-    //               <button className ='Button_Submit' type='Submit'> SUBMIT</button>
-    //             </div>
-
-    //           </div>
-
-    //         </div>
-    //      </div>
-    // </div>
-//   )
-// }
-
-
 const DetailAddTwo = () => {
   const navigate = useNavigate();
   const [phoneNo, setPhoneNo] = useState('');
   const [emailId, setEmailId] = useState('');
   const [linkedIn, setLinkedIn] = useState('');
   const [resume, setResume] = useState(null);
-
-  // useEffect(() => {
-  //   // Fetch the related user object and set the user id state
-  //   axiosInstance.get(`${baseUrl}/current-user/`)
-  //     .then(response => {
-  //       setUserId(response.data.username);
-  //       setId(response.data.id);
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -89,7 +33,6 @@ const DetailAddTwo = () => {
     if(response.status===201){
       toast.success("Message sent successfully!!!")
         navigate("/resume-upload")
-      //   window.location.reload();
       }
   })
   .catch((error) => {
