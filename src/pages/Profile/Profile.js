69c57b0314a3a0cc3322857d4ec349db5839d4fd
SnehaDevan <sdkvekm2@gmail.com>
2023-05-08 15:06:34 +0530

    Profile changed

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -3,32 +3,9 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { baseUrl } from '../../utils/Urls';
 import './Profile.css'
-import back from "../../assets/Profile_Background.jpeg"
-import pic from "../../assets/Profile_Pic2.jpeg"
+import back from "../../assets/Profile_Background.jpg"
+import pic from "../../assets/user.png"
 import axiosInstance from '../../auth/authHandler'
-
-// const Profile = () => {
-//   return (
-//     <div className='Profile_Container'>
-//        <div className='Profile_Content'>
-//             <div className='Profile_background'>
-//                 <img src={back} alt=''/>
-//             </div>
-//             <div className='Profile_All'>
-//                 <div className='Profile_Details'>
-//                       <h1>Candidate Name</h1>
-//                       <h2>City,State</h2>
-//                 </div>
-
-//                 <div className='Profile_pic'>
-//                     <img src={pic} alt=''/>
-//                 </div>
-//             </div>
-//       </div>
-//     </div>
-//   )
-// }
-
 
 const Profile = () => {
   // const [candidateDetails, setCandidateDetails] = useState(null);
@@ -67,22 +44,22 @@ const Profile = () => {
 
   return (
     <div className='Profile_Container'>
-      <div className='Profile_Content'>
-        <div className='Profile_background'>
-          <img src={back} alt=''/>
-        </div>
-        <div className='Profile_All'>
-             
-            <div className='Profile_Details'>
-              <h1>{userid}</h1>
-              <h2>{location1}</h2>
-            </div>
-          
-              <div className='Profile_pic'>
-                <img src={pic} alt=''/>
-              </div>
-        </div>
-      </div>
+          <div className='Profile_Content'>
+                <div className='Profile_background'>
+                  <img src={back} alt=''/>
+                </div>
+                <div className='Profile_All'>
+                    
+                        <div className='Profile_Details'>
+                          <h1>{userid}</h1>
+                          <h2>{location1}</h2>
+                        </div>
+                      
+                          <div className='Profile_pic'>
+                            <img src={pic} alt=''/>
+                          </div>
+                </div>
+          </div>
     </div>
   )
   }
