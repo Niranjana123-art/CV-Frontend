d5d868ea8ee2d609425f7b7cd719720a3983d404
SnehaDevan <sdkvekm2@gmail.com>
2023-05-29 20:28:58 +0530

    Aptitude Integretion

--- src/components/AptitudeSucess/AptitudeSuccess.js
+++ src/components/AptitudeSucess/AptitudeSuccess.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { createPortal } from 'react-dom'
 import './AptitudeSuccess.css'
 import SvgImage from  '../../assets/bgmodal.svg'
+import { useNavigate } from 'react-router-dom';
+
 const  AptitudeSuccess = ({open,setOpen}) => {
- if (!open){
-    return null
- }
- 
+//  if (!open){
+//     return null
+//  }
+  const navigate=useNavigate();
+  
   return createPortal(
     <div className='AptitudeSuccess_Container'>
      
@@ -15,7 +18,9 @@ const  AptitudeSuccess = ({open,setOpen}) => {
            Aptitude Test <br/>Submitted <span style={{color:'green'}}>Successfully!</span>
          </div>
            <div>
-           <button onClick={()=> setOpen(false)} className='AptitudeSuccess_button'>View Score</button>
+           <button onClick={() => {
+             navigate("/aptitude-score");
+            }} className='AptitudeSuccess_button'>View Score</button>
            </div>
     </div>
   </div>,
--- src/pages/AptitudeScore/AptitudeScore.js
+++ src/pages/AptitudeScore/AptitudeScore.js
@@ -1,10 +1,33 @@
-import React from 'react'
 import "./AptitudeScore.css"
 import {FiBookOpen} from "react-icons/fi";
 import {FaAddressCard} from "react-icons/fa";
 import {BsPersonCircle} from "react-icons/bs";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { baseUrl } from '../../utils/Urls';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../auth/authHandler';
 
 const AptitudeScore = () => {
+  const [score, setScore] = useState(null);
+  const navigate=useNavigate();
+  
+  
+  useEffect(() => {
+    fetchScore();
+  }, []);
+
+  const fetchScore = async () => {
+    try {
+
+      const response = await axios.get(`${baseUrl}/training/score`);
+      const scoreData = response.data.score;
+      setScore(scoreData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className='AptitudeScore_Container'>
             <div className='AptitudeScore_Box'>
@@ -12,35 +35,12 @@ const AptitudeScore = () => {
                    <FiBookOpen size={50}/>
                    <h2>What We Received From You</h2>
                 </div>
-                <div className='AptitudeScore_CandidateDetails'>
-                    <FaAddressCard size={100}/>
-                    <h1>Candidate Name</h1>
-                    <h2>City,State</h2>
-                </div>
                 <div className='AptitudeScore_CandidateScore'>
-                <h2>Maths</h2>
-                     <hr
-                        style={{
-                        background: 'black',
-                        color: 'black',
-                        borderColor: 'black',
-                        height: '1px',
-                        width: '400px',
-                        }}
-                     />
-                     <h2>English</h2>
-                     <hr
-                        style={{
-                        background: 'black',
-                        color: 'black',
-                        borderColor: 'black',
-                        height: '1px',
-                        width: '400px',
-                        }}
-                     />
+                     <h2>Aptitude Test Score</h2>
+                     <h2>You Scored {score} out of 5.</h2>
                 </div>
                 <div className='AptitudeScore_Button'>
-                     <button type="submit">FINISH</button>
+                     <button onClick={() => {navigate("/profile");}} type="submit">FINISH</button>
                 </div>
             </div>
     </div>
